8fbcf8d50525157eaf168e645ee6d02e179f39f5
unknown <handdara.core@gmail.com>
2023-10-03 17:35:07 -0400

    first commit

---
+++ ubuntu/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    function fish_prompt
+        printf '%s: %s%s%s > ' $USER \
+            (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    end
+
+    function fish_greeting
+        printf "This machine is %s%s%s. Lets fish...\n" (set_color green) $hostname (set_color normal)
+    end
+end
+
+fish_add_path ~/.cargo/bin
+fish_add_path ~/matlab/r2022b/bin
+fish_add_path /usr/local/texlive/2023/bin/x86_64-linux/
+fish_add_path ~/.local/bin
+
+# ------------------ ALIASES ------------------
+
+alias e "exa"
+alias ea "exa -a"
+alias el "exa -al"
+alias zdi "zellij -s dothe"
+
+# ------------------ END ALIASES ------------------
+
+sh ~/.cargo/env
