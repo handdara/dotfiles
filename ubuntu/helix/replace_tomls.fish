db4d93007b305917d5cfdd094c0f8640ee596290
nbrittain3 <nbrittain3@gatech.edu>
2023-10-18 21:26:48 -0400

    wsl2 fix for latex fwd search/build

--- ubuntu/helix/replace_tomls.fish
+++ ubuntu/helix/replace_tomls.fish
@@ -1,3 +1,9 @@
 
-cp ./*.toml ~/.config/helix/
 
+if set -q WSL_DISTRO_NAME[1]
+  echo "Writing wsl version"
+  cp ./wsl2/*.toml ~/.config/helix/
+else
+  echo "Writing native ubuntu versions"
+  cp ./*.toml ~/.config/helix/
+end
