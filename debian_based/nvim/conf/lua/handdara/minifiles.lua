e242d027bd116afa742187aabcce86b4f3da79dd
handdara <handdara.core@gmail.com>
2023-12-01 13:21:07 -0500

    add mini.files keymaps

--- debian_based/nvim/conf/lua/handdara/minifiles.lua
+++ debian_based/nvim/conf/lua/handdara/minifiles.lua
@@ -1,1 +1,15 @@
-require('mini.files').setup()
+require('mini.files').setup({
+  mappings = {
+    close       = 'q',
+    go_in       = 'l',
+    go_in_plus  = 'L',
+    go_out      = 'h',
+    go_out_plus = 'H',
+    reset       = '<BS>',
+    reveal_cwd  = 'g!',
+    show_help   = 'g?',
+    synchronize = '=',
+    trim_left   = '<',
+    trim_right  = '>',
+  },
+})
