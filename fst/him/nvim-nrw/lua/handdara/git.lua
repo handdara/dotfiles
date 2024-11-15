c2a6df8d76a5a6c11fc10cf2a6463d628d4cb90f
handdara <handdara.core@proton.me>
2024-11-15 18:13:16 -0500

    oopsies

--- fst/him/nvim-nrw/lua/handdara/git.lua
+++ fst/him/nvim-nrw/lua/handdara/git.lua
@@ -1,9 +1,9 @@
 local neogit = require 'neogit'
 local map = vim.keymap.set
 
-map('n', '<leader>gg', function ()
+map('n', '<leader>gg', function()
     neogit.open()
-end, {desc = 'Open neo[g]it'})
-map('n', '<leader>gs', function ()
-    neogit.open()
-end, {desc = 'Open neo[g]it in vertical [s]plit'})
+end, { desc = 'Open neo[g]it' })
+map('n', '<leader>gs', function()
+    neogit.open({ kind = 'vsplit' })
+end, { desc = 'Open neo[g]it in vertical [s]plit' })
