7eee47b318a40abe7b941c111aab1ffdc0db6075
handdara <handdara.core@proton.me>
2024-11-20 17:35:50 -0500

    Started function to make dev workspace

--- fst/hez/wezterm-main/handdara/workspaces/init.lua
+++ fst/hez/wezterm-main/handdara/workspaces/init.lua
@@ -8,56 +8,73 @@ table.insert(note_args, '-c')
 -- table.insert(note_args, 'nvim .')
 table.insert(note_args, 'nvim quicklinks.md')
 
+---makes a development workspace based on alphabetic key
+---@param key string
+---@return table
+local function mk_dev_wksp(key)
+    -- assert()
+    local key_num = string.byte(key,1,1)
+    -- local isalpha = (key_num 
+    local x = (string.len(key) == 1) && (key[1] )
+    local dev_wksp = {
+        name = 'dev' .. key,
+        spawn = {
+            domain = hd.dev_domain,
+        },
+    }
+    return dev_wksp
+end
+
 return {
-  config = {
-    name = 'config',
-    spawn = { cwd = hDirs.home_dir .. '/code/dotfiles' }
-  },
-  development = {
-    name = 'development',
-    spawn = {
-      domain = hd.dev_domain,
+    config = {
+        name = 'config',
+        spawn = { cwd = hDirs.home_dir .. '/code/dotfiles' }
     },
-  },
-  misc = { name = 'miscellaneous' },
-  hpi = {
-    name = 'hpi',
-    spawn = {
-      args = { 'fish', '-c', 'hpiconnect' },
-      domain = 'DefaultDomain',
+    development = {
+        name = 'development',
+        spawn = {
+            domain = hd.dev_domain,
+        },
     },
-  },
-  spotify = {
-    name = 'spotify',
-    spawn = {
-      args = { 'fish', '-c', 'spt' },
-      domain = 'DefaultDomain',
+    misc = { name = 'miscellaneous' },
+    hpi = {
+        name = 'hpi',
+        spawn = {
+            args = { 'fish', '-c', 'hpiconnect' },
+            domain = 'DefaultDomain',
+        },
     },
-  },
-  monitoring = {
-    name = 'monitoring',
-    spawn = {
-      args = hl.default_monitor.args,
-      domain = hd.monitor_domain,
+    spotify = {
+        name = 'spotify',
+        spawn = {
+            args = { 'fish', '-c', 'spt' },
+            domain = 'DefaultDomain',
+        },
     },
-  },
-  work_notes = {
-    name = 'work-notes',
-    spawn = {
-      args = note_args,
-      cwd = hDirs.ansible_dir .. '/2-areas/work',
-      -- domain = hd.notes_domain, -- debugging issue with separate domains
-      domain = hd.dev_domain,
+    monitoring = {
+        name = 'monitoring',
+        spawn = {
+            args = hl.default_monitor.args,
+            domain = hd.monitor_domain,
+        },
     },
-  },
-  personal_notes = {
-    name = 'personal-notes',
-    spawn = {
-      args = note_args,
-      cwd = hDirs.ansible_dir,
-      -- domain = hd.notes_domain,
-      domain = hd.dev_domain,
+    work_notes = {
+        name = 'work-notes',
+        spawn = {
+            args = note_args,
+            cwd = hDirs.ansible_dir .. '/2-areas/work',
+            -- domain = hd.notes_domain, -- debugging issue with separate domains
+            domain = hd.dev_domain,
+        },
     },
-  },
-  mega = require('handdara.workspaces.mega'),
+    personal_notes = {
+        name = 'personal-notes',
+        spawn = {
+            args = note_args,
+            cwd = hDirs.ansible_dir,
+            -- domain = hd.notes_domain,
+            domain = hd.dev_domain,
+        },
+    },
+    mega = require('handdara.workspaces.mega'),
 }
