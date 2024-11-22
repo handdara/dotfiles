87694ecf14717f43be3d96f262d39a34d00fed99
handdara <handdara.core@proton.me>
2024-11-22 14:54:08 -0500

    Squashed commit of the following:
    commit c12890aa97499dcb7170ce3bc144ea90aa15eaa2
    Author: handdara <handdara.core@proton.me>
    Date:   Fri Nov 22 14:53:34 2024 -0500
    
        Minor
    
    commit 6e74ec2a657bb17e57fc78fce7989dea72b6c173
    Author: handdara <handdara.core@proton.me>
    Date:   Fri Nov 22 02:40:38 2024 -0500
    
        First working wezterm key-map & workspace rebuild
    
    commit 4d5ad017730218e3605a5381f77599d9221d6c51
    Merge: 7eee47b 0617685
    Author: handdara <handdara.core@proton.me>
    Date:   Thu Nov 21 17:32:31 2024 -0500
    
        Merge branch 'trunk' into wezterm-wksp-improvements
    
    commit 7eee47b318a40abe7b941c111aab1ffdc0db6075
    Author: handdara <handdara.core@proton.me>
    Date:   Wed Nov 20 17:35:50 2024 -0500
    
        Started function to make dev workspace
    
    commit 4b0361a987115d874f807d36a9bd148739e18bd9
    Author: handdara <handdara.core@proton.me>
    Date:   Tue Nov 19 23:30:52 2024 -0500
    
        Improve wezterm justfile
    
        - added wezterm/hez to dev mode
        - added target to hez justfile
        - set fallback for hez justfile

--- fst/hez/wezterm-main/handdara/workspaces/init.lua
+++ fst/hez/wezterm-main/handdara/workspaces/init.lua
@@ -5,59 +5,76 @@ local hd = require 'handdara.domains'
 local note_args = {}
 table.insert(note_args, hl.system_shell.args[1])
 table.insert(note_args, '-c')
--- table.insert(note_args, 'nvim .')
 table.insert(note_args, 'nvim quicklinks.md')
 
+---makes a development workspace based on alphabetic key
+---@param key string which key (a through z) to id the wksp with
+---@return table
+local function mk_dev_wksp(key)
+    assert(type(key) == 'string')
+    local key_lower = string.lower(key)
+    local key_num = string.byte(key_lower, 1, 1)
+    local isalpha = (97 <= key_num and key_num <= 122) -- a:97 z:122 A:65 Z:90
+    assert((string.len(key_lower) == 1) and isalpha)
+    return {
+        name = 'dev-' .. key_lower,
+        spawn = {
+            domain = hd.dev_domain,
+        },
+    }
+end
+
 return {
-  config = {
-    name = 'config',
-    spawn = { cwd = hDirs.home_dir .. '/code/dotfiles' }
-  },
-  development = {
-    name = 'development',
-    spawn = {
-      domain = hd.dev_domain,
+    config = {
+        name = 'config',
+        spawn = { cwd = hDirs.home_dir .. '/code/dotfiles' }
     },
-  },
-  misc = { name = 'miscellaneous' },
-  hpi = {
-    name = 'hpi',
-    spawn = {
-      args = { 'fish', '-c', 'hpiconnect' },
-      domain = 'DefaultDomain',
+    development = {
+        name = 'development',
+        spawn = {
+            domain = hd.dev_domain,
+        },
     },
-  },
-  spotify = {
-    name = 'spotify',
-    spawn = {
-      args = { 'fish', '-c', 'spt' },
-      domain = 'DefaultDomain',
+    misc = { name = 'miscellaneous' },
+    hpi = {
+        name = 'hpi',
+        spawn = {
+            args = { 'fish', '-c', 'hpiconnect' },
+            domain = 'DefaultDomain',
+        },
     },
-  },
-  monitoring = {
-    name = 'monitoring',
-    spawn = {
-      args = hl.default_monitor.args,
-      domain = hd.monitor_domain,
+    spotify = {
+        name = 'spotify',
+        spawn = {
+            args = { 'fish', '-c', 'spt' },
+            domain = 'DefaultDomain',
+        },
     },
-  },
-  work_notes = {
-    name = 'work-notes',
-    spawn = {
-      args = note_args,
-      cwd = hDirs.ansible_dir .. '/2-areas/work',
-      -- domain = hd.notes_domain, -- debugging issue with separate domains
-      domain = hd.dev_domain,
+    monitoring = {
+        name = 'monitoring',
+        spawn = {
+            args = hl.default_monitor.args,
+            domain = hd.monitor_domain,
+        },
     },
-  },
-  personal_notes = {
-    name = 'personal-notes',
-    spawn = {
-      args = note_args,
-      cwd = hDirs.ansible_dir,
-      -- domain = hd.notes_domain,
-      domain = hd.dev_domain,
+    work_notes = {
+        name = 'work-notes',
+        spawn = {
+            args = note_args,
+            cwd = hDirs.ansible_dir .. '/2-areas/work',
+            -- domain = hd.notes_domain, -- debugging issue with separate domains
+            domain = hd.dev_domain,
+        },
     },
-  },
-  mega = require('handdara.workspaces.mega'),
+    personal_notes = {
+        name = 'personal-notes',
+        spawn = {
+            args = note_args,
+            cwd = hDirs.ansible_dir,
+            -- domain = hd.notes_domain,
+            domain = hd.dev_domain,
+        },
+    },
+    mega = require('handdara.workspaces.mega'),
+    mk_dev_wksp = mk_dev_wksp,
 }
