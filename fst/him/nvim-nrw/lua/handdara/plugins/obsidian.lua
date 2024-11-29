77f033a5df09155db2525fda9847a23d3f1af9c1
handdara <handdara.core@proton.me>
2024-11-29 17:35:33 -0500

    Added pomo.nvim

--- fst/him/nvim-nrw/lua/handdara/plugins/obsidian.lua
+++ fst/him/nvim-nrw/lua/handdara/plugins/obsidian.lua
@@ -1,56 +1,56 @@
 ---@diagnostic disable: unused-local
 local ansible_path = vim.fn.expand("~/MEGA/ansible")
 
--- local pomo_opts = {
---     -- How often the notifiers are updated.
---     update_interval = 1000,
---     -- Configure the default notifiers to use for each timer.
---     -- You can also configure different notifiers for timers given specific names, see
---     -- the 'timers' field below.
---     notifiers = {
---         -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
---         {
---             name = "Default",
---             opts = {
---                 -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
---                 -- continuously displayed. If you only want a pop-up notification when the timer starts
---                 -- and finishes, set this to false.
---                 sticky = true,
---                 -- Configure the display icons:
---                 title_icon = "",
---                 text_icon = "",
---                 -- Replace the above with these if you don't have a patched font:
---                 -- title_icon = "⏳",
---                 -- text_icon = "⏱️",
---             },
---         },
---         -- The "System" notifier sends a system notification when the timer is finished.
---         -- Available on MacOS and Windows natively and on Linux via the `libnotify-bin` package.
---         { name = "System" },
---         -- You can also define custom notifiers by providing an "init" function instead of a name.
---         -- { init = function(timer) ... end }
---     },
---     -- Override the notifiers for specific timer names.
---     timers = {
---         -- For example, use only the "System" notifier when you create a timer called "Break",
---         -- e.g. ':TimerStart 2m Break'.
---         Break = {
---             { name = "System" },
---         },
---     },
---     -- You can optionally define custom timer sessions.
---     sessions = {
---         -- Example session configuration for a session called "pomodoro".
---         pomodoro = {
---             { name = "Work", duration = "25m" },
---             { name = "Short Break", duration = "5m" },
---             { name = "Work", duration = "25m" },
---             { name = "Short Break", duration = "5m" },
---             { name = "Work", duration = "25m" },
---             { name = "Long Break", duration = "15m" },
---         },
---     },
--- }
+local pomo_opts = {
+    -- How often the notifiers are updated.
+    update_interval = 1000,
+    -- Configure the default notifiers to use for each timer.
+    -- You can also configure different notifiers for timers given specific names, see
+    -- the 'timers' field below.
+    notifiers = {
+        -- The "Default" notifier uses 'vim.notify' and works best when you have 'nvim-notify' installed.
+        {
+            name = "Default",
+            opts = {
+                -- With 'nvim-notify', when 'sticky = true' you'll have a live timer pop-up
+                -- continuously displayed. If you only want a pop-up notification when the timer starts
+                -- and finishes, set this to false.
+                sticky = true,
+                -- Configure the display icons:
+                title_icon = "",
+                text_icon = "",
+                -- Replace the above with these if you don't have a patched font:
+                -- title_icon = "⏳",
+                -- text_icon = "⏱️",
+            },
+        },
+        -- The "System" notifier sends a system notification when the timer is finished.
+        -- Available on MacOS and Windows natively and on Linux via the `libnotify-bin` package.
+        { name = "System" },
+        -- You can also define custom notifiers by providing an "init" function instead of a name.
+        -- { init = function(timer) ... end }
+    },
+    -- Override the notifiers for specific timer names.
+    timers = {
+        -- For example, use only the "System" notifier when you create a timer called "Break",
+        -- e.g. ':TimerStart 2m Break'.
+        Break = {
+            { name = "System" },
+        },
+    },
+    -- You can optionally define custom timer sessions.
+    sessions = {
+        -- Example session configuration for a session called "pomodoro".
+        pomodoro = {
+            { name = "Work", duration = "25m" },
+            { name = "Short Break", duration = "5m" },
+            { name = "Work", duration = "25m" },
+            { name = "Short Break", duration = "5m" },
+            { name = "Work", duration = "25m" },
+            { name = "Long Break", duration = "15m" },
+        },
+    },
+}
 
 return {
     "epwalsh/obsidian.nvim",
@@ -64,14 +64,14 @@ return {
         'hrsh7th/nvim-cmp',
         'nvim-telescope/telescope.nvim',
         'nvim-treesitter/nvim-treesitter',
-        -- {
-        --     "epwalsh/pomo.nvim",
-        --     version = "*", -- Recommended, use latest release instead of latest commit
-        --     lazy = true,
-        --     cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
-        --     opts = pomo_opts,
-        -- },
-        -- 'rcarriga/nvim-notify', -- for pomo.nvim, optional, but highly recommended if you want to use the "Default" timer
+        {
+            "epwalsh/pomo.nvim",
+            version = "*", -- Recommended, use latest release instead of latest commit
+            lazy = true,
+            cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+            opts = pomo_opts,
+        },
+        'rcarriga/nvim-notify', -- for pomo.nvim, optional, but highly recommended if you want to use the "Default" timer
     },
     init = function()
         require 'handdara.obsidian'
