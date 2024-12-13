4ac3d4ee3cb2e14778afb10444720d44ebd7486f
handdara <handdara.core@proton.me>
2024-12-12 23:35:04 -0500

    better reds

--- hix/util/color/marrissa-term.nix
+++ hix/util/color/marrissa-term.nix
@@ -4,7 +4,7 @@
 
   # normal:
   black   = "#2b2b2b";
-  red     = "#783028"; # original red hexcode: #4f1913
+  red     = "#8A2C24"; # original red hexcode: #4f1913
   green   = "#92bd33";
   yellow  = "#e4a442";
   blue    = "#3f5a6e";
@@ -14,7 +14,7 @@
 
   # Bright colors
   bright_black    = "#363b49";
-  bright_red      = "#700c01";
+  bright_red      = "#84150B";
   bright_green    = "#a0c54c";
   bright_yellow   = "#ffc66d";
   bright_blue     = "#6d9cbe";
