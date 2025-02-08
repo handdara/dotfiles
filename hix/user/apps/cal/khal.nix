55aa241cb9c6001cf5aecc7e7cdb34563c4c585a
handdara <handdara.core@proton.me>
2025-02-08 00:18:30 -0500

    8feb2025 00:18

--- hix/user/apps/cal/khal.nix
+++ hix/user/apps/cal/khal.nix
@@ -4,9 +4,14 @@
         ".config/khal/config".text = ''
           [calendars]
           [[obl]]
+          priority = 20
           path = ~/MEGA/ansible/6-assets/icloud_calendar/home
           [[rem]]
+          priority = 20
           path = ~/MEGA/ansible/6-assets/icloud_calendar/A20717B0-A788-4C5E-947F-817AF7457F97
+          [keybindings]
+          save = s
+          external_edit = ctrl e
         '';
     };
 }
