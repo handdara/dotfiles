8a7861ab5f034e79771d19c0e1aecbe7e82c19d4
handdara <handdara.core@proton.me>
2025-02-10 18:04:56 -0500

    Changed khal date/time formatting

--- hix/user/apps/cal/khal.nix
+++ hix/user/apps/cal/khal.nix
@@ -9,9 +9,17 @@
           [[rem]]
           priority = 20
           path = ~/MEGA/ansible/6-assets/icloud_calendar/A20717B0-A788-4C5E-947F-817AF7457F97
+          [default]
+          print_new = event
           [keybindings]
           save = ctrl s
           external_edit = ctrl e
+          [locale]
+          timeformat = "%H%M"
+          dateformat = "%d%b%Y"
+          datetimeformat = "%d%b%Y %H%M"
+          longdateformat = "%d%b%Y"
+          longdatetimeformat = "%d%b%Y %H%M"
         '';
     };
 }
