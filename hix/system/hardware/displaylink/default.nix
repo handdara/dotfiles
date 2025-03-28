841f4c180e732ec526ea75eecc9cae3cb36e83ce
handdara <handdara.core@proton.me>
2025-03-28 15:30:25 -0400

    Add nvidia offloading support to ccrf nixos specialization

--- hix/system/hardware/displaylink/default.nix
+++ hix/system/hardware/displaylink/default.nix
@@ -18,9 +18,23 @@
         # mv $PWD/"DisplayLink USB Graphics Software for Ubuntu5.8-EXE.zip" $PWD/displaylink-580.zip
         # nix-prefetch-url file://$PWD/displaylink-580.zip
         # ***
-        services.xserver.videoDrivers = ["displaylink" "modesetting"];
+        services.xserver.videoDrivers = ["displaylink" "modesetting" "nvidia"];
         services.xserver.displayManager.sessionCommands = ''
           ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
         '';
+
+        # see also hix/machines/sha76/gpu.nix for another use of this code, it's also where I copied it from
+        hardware.graphics.enable = true;
+        hardware.nvidia.open = false; # Set to false to use the proprietary kernel module
+        hardware.nvidia.prime = {
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+            offload = {
+                enable = true;
+                enableOffloadCmd = true;
+            };
+        };
+        system.nixos.tags = ["nvidia" "offload" "ccrf"];
+        services.xserver.desktopManager.gnome.enable = true;
     };
 }
