4838cdccc350a28516fd824d50cd2f9e1e06f7c9
handdara <handdara.core@proton.me>
2025-05-02 15:13:08 -0400

    2May2025 15:13

--- fst/him/nvim-nrw/lua/handdara/snippets/markdown.lua
+++ fst/him/nvim-nrw/lua/handdara/snippets/markdown.lua
@@ -222,80 +222,102 @@ local sdaily = s('daily', d(1, function()
 end))
 use(sdaily)
 
-local bweekly = [[
-### WK{1}-2025, week of {4}
+-- local bweekly = [[
+-- ### WK{1}-2025, week of {4}
+--
+-- | M      | T      | W           | R    | F    | S     | N   |
+-- |--------|--------|-------------|------|------|-------|-----|
+-- | thesis | thesis | thesis/seal | seal | seal | piano |     |
+--
+-- #### affirms & active investments
+-- #todo, see {2}
+--
+-- #### goals
+-- #todo, see {3}
+--
+-- #### reflection
+--
+-- ##### wheel of life
+--
+-- health:
+-- -   body
+--     -   how im doing in general: {5}
+--     -   how well i did this past week: {6}
+-- -   mind
+--     -   how im doing in general: {7}
+--     -   how well i did this past week: {8}
+-- -   soul
+--     -   how im doing in general: {9}
+--     -   how well i did this past week: {10}
+-- work:
+-- -   mission
+--     -   how im doing in general: {11}
+--     -   how well i did this past week: {12}
+-- -   money
+--     -   how im doing in general: {13}
+--     -   how well i did this past week: {14}
+-- -   growth
+--     -   how im doing in general: {15}
+--     -   how well i did this past week: {16}
+-- relationships:
+-- -   family
+--     -   how im doing in general: {17}
+--     -   how well i did this past week: {18}
+-- -   romance
+--     -   how im doing in general: {19}
+--     -   how well i did this past week: {20}
+-- -   friends
+--     -   how im doing in general: {21}
+--     -   how well i did this past week: {22}
+--
+-- ]]
+local bweekly = [=[
+## planning for WK{1}
 
-| M      | T      | W           | R    | F    | S     | N   |
-|--------|--------|-------------|------|------|-------|-----|
-| thesis | thesis | thesis/seal | seal | seal | piano |     |
+- [ ]   calculating remaining hours needed at gtri for the month
+- [ ]   reflecting on the prev week
+    -   including the wheel of life exercise
+- [ ]   reviewing [[affirms-vals-goals]] and setting one affirmation in my wkly affirm iphone note
+- [ ]   reviewing quarterly goals (see [[quarterly-goals-2025#spring-goals]])
+- [ ]   review calendar for the week
+- [ ]   review  task board:
+    ```lua
+    Handdara.stache.task_board()
+    ```
+- [ ]   go through pocket notebook for unfinished tasks
+- [ ]   set out weekly goals
+    - [ ]   setup alistair page for goals and add [[weekly-quests]] to it
+        -   i have a list for [[my-space#weekly-upkeep]] on my watch
+        -   includ hours of work each day
+- [ ]   clear garmin weekly upkeep checklist
+- [ ]   plan meals/exercise for the week
 
-#### affirms & active investments
-#todo, see {2}
-
-#### goals
-#todo, see {3}
-
-#### reflection
-
-##### wheel of life
-
-health:
--   body
-    -   how im doing in general: {5}
-    -   how well i did this past week: {6}
--   mind
-    -   how im doing in general: {7}
-    -   how well i did this past week: {8}
--   soul
-    -   how im doing in general: {9}
-    -   how well i did this past week: {10}
-work:
--   mission
-    -   how im doing in general: {11}
-    -   how well i did this past week: {12}
--   money
-    -   how im doing in general: {13}
-    -   how well i did this past week: {14}
--   growth
-    -   how im doing in general: {15}
-    -   how well i did this past week: {16}
-relationships:
--   family
-    -   how im doing in general: {17}
-    -   how well i did this past week: {18}
--   romance
-    -   how im doing in general: {19}
-    -   how well i did this past week: {20}
--   friends
-    -   how im doing in general: {21}
-    -   how well i did this past week: {22}
-
-]]
+]=]
 local dweekly = d(1, function()
     local ts = u.timestamp()
     return sn(nil, fmt(bweekly, {
         t(u.dtnum2str( ts.wk+1 )),
-        t({ '[[affirms-vals-goals]]', 'also [[quarterly-goals-2025#list of possible personal investments]]' }),
-        t('[[quarterly-goals-2025#Quarter ' .. ts.qt .. ' Goals]]'),
-        i(1, 'DDmmmYYYY'),
-        c(2, {t'1', t'2', t'3', t'4', }),
-        c(3, {t'1', t'2', t'3', t'4', }),
-        c(4, {t'1', t'2', t'3', t'4', }),
-        c(5, {t'1', t'2', t'3', t'4', }),
-        c(6, {t'1', t'2', t'3', t'4', }),
-        c(7, {t'1', t'2', t'3', t'4', }),
-        c(8, {t'1', t'2', t'3', t'4', }),
-        c(9, {t'1', t'2', t'3', t'4', }),
-        c(10, {t'1', t'2', t'3', t'4', }),
-        c(11, {t'1', t'2', t'3', t'4', }),
-        c(12, {t'1', t'2', t'3', t'4', }),
-        c(13, {t'1', t'2', t'3', t'4', }),
-        c(14, {t'1', t'2', t'3', t'4', }),
-        c(15, {t'1', t'2', t'3', t'4', }),
-        c(16, {t'1', t'2', t'3', t'4', }),
-        c(17, {t'1', t'2', t'3', t'4', }),
-        c(18, {t'1', t'2', t'3', t'4', }),
-        c(19, {t'1', t'2', t'3', t'4', }),
+        -- t({ '[[affirms-vals-goals]]', 'also [[quarterly-goals-2025#list of possible personal investments]]' }),
+        -- t('[[quarterly-goals-2025#Quarter ' .. ts.qt .. ' Goals]]'),
+        -- i(1, 'DDmmmYYYY'),
+        -- c(2, {t'1', t'2', t'3', t'4', }),
+        -- c(3, {t'1', t'2', t'3', t'4', }),
+        -- c(4, {t'1', t'2', t'3', t'4', }),
+        -- c(5, {t'1', t'2', t'3', t'4', }),
+        -- c(6, {t'1', t'2', t'3', t'4', }),
+        -- c(7, {t'1', t'2', t'3', t'4', }),
+        -- c(8, {t'1', t'2', t'3', t'4', }),
+        -- c(9, {t'1', t'2', t'3', t'4', }),
+        -- c(10, {t'1', t'2', t'3', t'4', }),
+        -- c(11, {t'1', t'2', t'3', t'4', }),
+        -- c(12, {t'1', t'2', t'3', t'4', }),
+        -- c(13, {t'1', t'2', t'3', t'4', }),
+        -- c(14, {t'1', t'2', t'3', t'4', }),
+        -- c(15, {t'1', t'2', t'3', t'4', }),
+        -- c(16, {t'1', t'2', t'3', t'4', }),
+        -- c(17, {t'1', t'2', t'3', t'4', }),
+        -- c(18, {t'1', t'2', t'3', t'4', }),
+        -- c(19, {t'1', t'2', t'3', t'4', }),
     }))
 end)
 use(s('wkly', dweekly))
