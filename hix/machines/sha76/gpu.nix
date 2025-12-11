0bb2fb20d22b19545617d8a87315f5c5f94448d3
handdara <handdara.core@proton.me>
2025-12-10 21:15:43 -0500

    Pre-switch, __h_switch_nixos commited generation 121 @1765419343
    Generation 121, NixOS Version 26.05.20251130.2d293cb (Kernel 6.12.59), built on 2025-12-08 02:14:51.

--- hix/machines/sha76/gpu.nix
+++ hix/machines/sha76/gpu.nix
@@ -1,29 +1,12 @@
 {lib, ...}: {
-    specialisation.nvidia-offload.configuration = {
-        hardware.graphics.enable = true;
-        services.xserver.videoDrivers = ["nvidia"];
-        hardware.nvidia.open = false; # Set to false to use the proprietary kernel module
-        # boot.kernelParams = ["module_blacklist=i915"];
-        hardware.nvidia.prime = {
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
-            offload = {
-                enable = true;
-                enableOffloadCmd = true;
-            };
-        };
-        system.nixos.tags = ["nvidia" "offload"];
-    };
     specialisation.nvidia-sync.configuration = {
-        hardware.graphics.enable = true;
-        services.xserver.videoDrivers = ["nvidia"];
-        hardware.nvidia.open = false; # Set to false to use the proprietary kernel module
-        # boot.kernelParams = ["module_blacklist=i915"];
         hardware.nvidia.prime = {
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
             sync.enable = true;
             # reverseSync.enable = true;
+        };
+        offload = {
+            enable = lib.mkForce false;
+            enableOffloadCmd = lib.mkForce false;
         };
         system.nixos.tags = ["nvidia" "sync"];
     };
