2f1ffe87417d1f82829191ba009af3faef6c37a4
handdara <email@handdara.com>
2025-12-13 13:40:17 -0500

    changing default mg keybind to use xterm for speed

--- hix/user/apps/x/default.nix
+++ hix/user/apps/x/default.nix
@@ -3,19 +3,19 @@
     ...
 }: {
     home.file.".Xresources".text = ''
-        XTerm*faceName: ${config.handdara.fontnone or "Monospace"}
+        XTerm*faceName: ${config.handdara.font or "Monospace"}
         XTerm*faceSize: ${builtins.toString (config.handdara.fontsize + 2)}
         XTerm*foreground: ${
             if (config.handdara.lightworks)
             then "black"
             else "white"
         }
-        XTerm*background: black ${
+        XTerm*background: ${
             if (config.handdara.lightworks)
             then "white"
             else "black"
         }
-        XTerm*cursorColor: white ${
+        XTerm*cursorColor: ${
             if (config.handdara.lightworks)
             then "black"
             else "white"
