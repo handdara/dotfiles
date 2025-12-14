66a2e8152ea9bda3b7ac24375bbb96e66bdbbff5
handdara <email@handdara.com>
2025-12-14 12:52:36 -0500

    Pre-switch, __h_switch_home_mgr commited mixed generation id 27 @1765734756
    2025-12-14 12:49 : id 27 -> /nix/store/c6120d8wg203g2qqjw7yhw3s11asqm1d-home-manager-generation (current)

--- hix/user/apps/ghostty/default.nix
+++ hix/user/apps/ghostty/default.nix
@@ -40,7 +40,7 @@ in {
             font-size = ${builtins.toString config.handdara.fontsize}
             cursor-style = block
             cursor-style-blink = false
-            background-opacity = 0.91
+            background-opacity = 0.92
             window-padding-x = 2
             window-padding-y = 0
             window-decoration = false
@@ -52,7 +52,8 @@ in {
             keybind = ctrl+shift+a=unbind
             keybind = ctrl+alt+shift+t=toggle_tab_overview
             keybind = ctrl+alt+minus=toggle_tab_overview
-            keybind = global:super+shift+enter=toggle_quick_terminal
+            keybind = global:super+alt+shift+t=toggle_quick_terminal
+            keybind = ctrl+alt+shift+enter=toggle_quick_terminal
         '';
         ".config/ghostty/themes/${name}".text = ''
             palette =  0=${theme.ghostty.palette0 or c.black}
