324b1ba50222b8df943a9f7fc03798d079bc29c4
handdara <handdara.core@proton.me>
2025-12-14 00:55:46 -0500

    Post-switch, __h_switch_nixos commited sha76 generation 123 @1765691781
    Generation 123, NixOS Version nvidia-offload-26.05.20251130.2d293cb (Kernel 6.12.59), built on 2025-12-13 22:32:58.

--- hix/machines/sha76/gpu.nix
+++ hix/machines/sha76/gpu.nix
@@ -1,4 +1,16 @@
 {lib, ...}: {
+    hardware.graphics.enable = lib.mkDefault true;
+    services.xserver.videoDrivers = lib.mkDefault ["nvidia"];
+    hardware.nvidia.open = lib.mkDefault false; # Set to false to use the proprietary kernel module
+    # boot.kernelParams = ["module_blacklist=i915"];
+    hardware.nvidia.prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+        offload = {
+            enable = lib.mkForce true;
+            enableOffloadCmd = lib.mkForce true;
+        };
+    };
     specialisation.nvidia-sync.configuration = {
         hardware.nvidia.prime = {
             sync.enable = true;
