59015cc2147ffeeb3c49facfb81122d6d3934b1f
handdara <handdara.core@proton.me>
2026-01-03 22:27:48 -0500

    Post-switch, __h_switch_nixos commited sha76 generation 136 @1767497283
    Generation 136, NixOS Version 26.05.20251130.2d293cb (Kernel 6.12.59), built on 2026-01-03 21:07:46.

--- hix/machines/sha76/gpu.nix
+++ hix/machines/sha76/gpu.nix
@@ -3,7 +3,7 @@
     services.xserver.videoDrivers = ["nvidia" "modesetting"];
     # boot.kernelParams = ["module_blacklist=i915"];
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
-    hardware.nvidia.open = true; # Set to false to use the proprietary kernel module
+    hardware.nvidia.open = false; # Set to false to use the proprietary kernel module
     hardware.nvidia.prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
