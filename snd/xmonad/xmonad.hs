87631973f2bcc3dd0c1e116fcc1bec26864b683c
handdara <handdara.core@proton.me>
2026-01-05 01:33:28 -0500

    Add start of xmonad cfg

--- snd/xmonad/xmonad.hs
+++ snd/xmonad/xmonad.hs
@@ -1,12 +1,14 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 
-myKeys = 
+keymaps = 
     [ ("M-<Return>", spawn "ghostty")
-    , ()
+    , ("M-S-r", spawn "xmonad --recompile" >> spawn "xmonad --restart")
+    , ("M-S-c", kill)
     ]
 
-main = xmonad def
+main = xmonad $ def
     { modMask = mod4Mask
     , terminal = "ghostty"
     }
+    `additionalKeysP` keymaps
