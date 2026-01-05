7cc8e224db7414787f1c607ddf28aee9d95350ab
handdara <handdara.core@proton.me>
2026-01-05 00:45:17 -0500

    Pre-switch, __h_switch_home_mgr commited sha76 generation id 1541 @1767591917
    2026-01-05 00:33 : id 1541 -> /nix/store/6j7rlchya2n2dfk6dbk9i6bf85b10caw-home-manager-generation (current)

--- snd/xmonad/xmonad.hs
+++ snd/xmonad/xmonad.hs
@@ -1,6 +1,12 @@
 import XMonad
+import XMonad.Util.EZConfig (additionalKeysP)
+
+myKeys = 
+    [ ("M-<Return>", spawn "ghostty")
+    , ()
+    ]
 
 main = xmonad def
-    -- { modMask = mod4Mask
-    -- , terminal = "ghostty"
-    -- }
+    { modMask = mod4Mask
+    , terminal = "ghostty"
+    }
