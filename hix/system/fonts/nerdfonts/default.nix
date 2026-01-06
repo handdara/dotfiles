d03fc481ead1e7022ac7cf72d09a1c91b0e50ab6
handdara <handdara.core@proton.me>
2026-01-05 19:46:42 -0500

    Pre-switch, __h_switch_nixos commited sha76 generation 147 @1767660402
    Generation 147, NixOS Version 26.05.20251130.2d293cb (Kernel 6.12.59), built on 2026-01-05 00:31:53.

--- hix/system/fonts/nerdfonts/default.nix
+++ hix/system/fonts/nerdfonts/default.nix
@@ -1,21 +1,22 @@
 {pkgs, ...}: {
     fonts.packages = with pkgs; [
-        nerd-fonts.monoid
-        nerd-fonts.hasklug
-        nerd-fonts.open-dyslexic
-        nerd-fonts.hurmit
-        nerd-fonts.im-writing
-        nerd-fonts.heavy-data
-        nerd-fonts.blex-mono
-        nerd-fonts.atkynson-mono
-        nerd-fonts.agave
+        maple-mono.NF
         nerd-fonts._3270
+        nerd-fonts.agave
+        nerd-fonts.atkynson-mono
+        nerd-fonts.bigblue-terminal
+        nerd-fonts.blex-mono
         nerd-fonts.departure-mono
         nerd-fonts.fantasque-sans-mono
+        nerd-fonts.hasklug
+        nerd-fonts.heavy-data
+        nerd-fonts.hurmit
+        nerd-fonts.im-writing
         nerd-fonts.monofur
-        nerd-fonts.bigblue-terminal
+        nerd-fonts.monoid
+        nerd-fonts.open-dyslexic
+        nerd-fonts.proggy-clean-tt
         nerd-fonts.terminess-ttf
-        maple-mono.NF
         recursive
     ];
 }
