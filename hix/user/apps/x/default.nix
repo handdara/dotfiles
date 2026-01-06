4e2e3d95d95d2b122779a0ce59831a9841050aee
handdara <handdara.core@proton.me>
2026-01-05 19:07:15 -0500

    Post-switch, __h_switch_nixos commited sha76 generation 147 @1767658338
    Generation 147, NixOS Version 26.05.20251130.2d293cb (Kernel 6.12.59), built on 2026-01-05 00:31:53.

--- hix/user/apps/x/default.nix
+++ hix/user/apps/x/default.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
     home.file.".Xresources".text = ''
         XTerm*faceName: ${config.handdara.font or "Monospace"}
-        XTerm*faceSize: ${builtins.toString (config.handdara.fontsize + 2)}
+        XTerm*faceSize: ${builtins.toString (config.handdara.fontsize or 14)}
         XTerm*foreground: ${
             if config.handdara.lightworks
             then "black"
