0dc14bd4ac0ad02f185b3d7fa6fd78ac0ed4d915
handdara <git@handdara.com>
2026-02-12 23:36:15 -0500

    WIP modded server

--- hix/machines/theseus/extra.nix
+++ hix/machines/theseus/extra.nix
@@ -1,3 +1,9 @@
 { ... }: {
   services.teamviewer.enable = true;
+  # In /etc/nixos/configuration.nix
+  virtualisation.docker = {
+    enable = true;
+  };
+  # Add to the docker group to run w/o sudo
+  users.users.estraven.extraGroups = [ "docker" ];
 }
