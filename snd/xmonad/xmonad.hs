cb8cc7c47e54ab2be52053882abe430b0bd74488
handdara <git@handdara.com>
2026-06-12 00:56:40 -0400

    formatting

--- snd/xmonad/xmonad.hs
+++ snd/xmonad/xmonad.hs
@@ -1,14 +1,16 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 
-keymaps = 
-    [ ("M-<Return>", spawn "ghostty")
-    , ("M-S-r", spawn "xmonad --recompile" >> spawn "xmonad --restart")
-    , ("M-S-c", kill)
-    ]
+keymaps =
+  [ ("M-<Return>", spawn "ghostty"),
+    ("M-S-r", spawn "xmonad --recompile" >> spawn "xmonad --restart"),
+    ("M-S-c", kill)
+  ]
 
-main = xmonad $ def
-    { modMask = mod4Mask
-    , terminal = "ghostty"
-    }
-    `additionalKeysP` keymaps
+main =
+  xmonad $
+    def
+      { modMask = mod4Mask,
+        terminal = "ghostty"
+      }
+      `additionalKeysP` keymaps
