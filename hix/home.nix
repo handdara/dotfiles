5c93bd627b6935322e37735050adc995a39a7db7
handdara <git@handdara.com>
2026-08-08 15:32:44 -0400

    Post-switch, __h_switch_nixos commited theseus generation 38 @1786217585
    Generation 38, NixOS Version 26.05.20260430.15f4ee4 (Kernel 6.18.26), built on 2026-08-02 13:45:53.

--- hix/home.nix
+++ hix/home.nix
@@ -5,7 +5,7 @@
 }: {
   handdara = rec {
     lightworks = false;
-    transparency = 85;
+    transparency = 30;
     font = "scientifica";
     # font = "MonaspiceAr NF";
     # font = "Maple Mono NF";
